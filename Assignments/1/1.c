645e8359db3579a1852ea938d00b23835c138e07
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-08-07 00:56:56 +0530

    Done with reversal. rewriting on screen midway

--- Assignments/1/1.c
+++ Assignments/1/1.c
@@ -1,23 +1,52 @@
-#include <stdio.h>
-#include <sys/stat.h>
+#include    <stdio.h>
+#include    <sys/stat.h>
+#include    <fcntl.h> 
+#include    <unistd.h>
+#include    <stdlib.h>
 
-void sys_print(char a*)
+void sys_print(char *a)
 {
     printf("%s\n", a);
 }
 
+int calcsize(char *file)
+{
+    struct stat statbuf;
+    stat(file, &statbuf);
+    return statbuf.st_size;
+}
+
 int main(int argc, char *argv[])
 {
-    if( argc <=2 ) {
+    if( argc < 2 ) {
        sys_print("No File Specified\n");
         return 1;
     }
 
-    struct stat statbuf;
+    //Size of the input
+    int size = calcsize( argv[1] );
 
-    stat(argv[1], &statbuf);
+    //Open the file
+    int fd = open(argv[1], O_RDONLY);
 
-    printf("%d is size of file\n", statbuf.st_size);
+    int fw = open("foo.txt", O_CREAT | O_WRONLY | O_APPEND);
+
+    if( fd < 0 || fw < 0) {
+        perror("Unavailable file");
+        return 1;
+    }
+
+    // Place input file pointer at end of file
+    lseek(fd, 0, 2);
+
+    for(int i=1; i<=size; i++) {
+        char c[1];
+        read(fd, c, 1);
+        write(fw, c, 1);
+        lseek(fd, -2, 1);
+        
+        write(1, "\rtest", 5);
+    }
 
     return 0;
 }
