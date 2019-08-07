cae6a6f112afecf6980a8a018580b521dfeb3ddd
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-08-08 02:12:32 +0530

    Nearly done

--- Assignments/1/1.c
+++ Assignments/1/1.c
@@ -3,6 +3,7 @@
 #include    <fcntl.h> 
 #include    <unistd.h>
 #include    <stdlib.h>
+#include    <string.h>
 
 void sys_print(char *a)
 {
@@ -13,7 +14,18 @@ int calcsize(char *file)
 {
     struct stat statbuf;
     stat(file, &statbuf);
+    // printf("%d\n", statbuf.st_size );
     return statbuf.st_size;
+}
+
+void strrev(char *str)
+{
+    for(int i=0; i<=strlen(str)/2; i++)
+    {
+        char temp = str[i];
+        str[i] = str[strlen(str)-i-1];
+        str[strlen(str)-i-1] = temp;
+    }
 }
 
 int main(int argc, char *argv[])
@@ -25,10 +37,12 @@ int main(int argc, char *argv[])
 
     //Size of the input
     int size = calcsize( argv[1] );
+    int chunksize = 5000;
 
     //Open the file
     int fd = open(argv[1], O_RDONLY);
 
+    //Open file to write too
     int fw = open("foo.txt", O_CREAT | O_WRONLY | O_APPEND);
 
     if( fd < 0 || fw < 0) {
@@ -37,15 +51,34 @@ int main(int argc, char *argv[])
     }
 
     // Place input file pointer at end of file
-    lseek(fd, 0, 2);
+    int preseek = (size>=5000) ? 5000 : size;
+    lseek(fd, -preseek, 2);
 
-    for(int i=1; i<=size; i++) {
-        char c[1];
-        read(fd, c, 1);
-        write(fw, c, 1);
-        lseek(fd, -2, 1);
+    //Go about the chunks of blocks
+    for(int i=0; i<=size; i+=5000) {
+
+        //Read 5000 or <5000 blocks?
+        int readsize = (size-i) >= 5000 ? 5000 : (size-i);
         
-        write(1, "\rtest", 5);
+        // printf("read: %d\n", readsize);
+
+        //Create string for reversal
+        char *c = malloc(readsize);
+        
+        read(fd, c, readsize);
+        // printf("%s\n", c);
+
+        //Reverse the string
+        strrev(c);
+        // printf("%s\n", c);
+
+        write(fw, c, readsize);
+        
+        lseek(fd, -6000, 1);
+
+        // printf("\r%f done", ((i*1.0)/size)*100);
+
+        free(c);
     }
 
     return 0;
