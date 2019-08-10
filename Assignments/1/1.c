958769991dbe60b98e9bfd6583e35f3163d3f92e
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-08-10 13:31:13 +0530

    Done with q1

--- Assignments/1/1.c
+++ Assignments/1/1.c
@@ -4,12 +4,16 @@
 #include    <unistd.h>
 #include    <stdlib.h>
 #include    <string.h>
+#include    <math.h>
+#include    <libgen.h>
 
+//Prints to screen
 void sys_print(char *a)
 {
-    printf("%s\n", a);
+    write(1, a, strlen(a));
 }
 
+//Calculates size of the file
 int calcsize(char *file)
 {
     struct stat statbuf;
@@ -17,6 +21,7 @@ int calcsize(char *file)
     return statbuf.st_size;
 }
 
+//Reverses the string
 void strrev(char *str, int size)
 {
     for(int i=0; i<=size/2; i++)
@@ -25,6 +30,17 @@ void strrev(char *str, int size)
         str[i] = str[size-i-1];
         str[size-i-1] = temp;
     }
+}
+
+//Moves the read file pointer back by the offset
+void movereadback(int fd, int offset) 
+{
+    lseek(fd, -offset, 1);
+}
+
+int calcprogress(float fraction)
+{
+    return floorl(fraction); 
 }
 
 int main(int argc, char *argv[])
@@ -36,46 +52,77 @@ int main(int argc, char *argv[])
 
     //Size of the input
     int size = calcsize( argv[1] );
-    int chunksize = 5000;
 
     //Open the file
     int fd = open(argv[1], O_RDONLY);
 
-    //Open file to write too
-    int fw = open("foo.txt", O_CREAT | O_WRONLY | O_APPEND);
-
-    if( fd < 0 || fw < 0) {
+    if( fd < 0) {
         perror("Unavailable file");
         return 1;
     }
 
-    // Place input file pointer at end of file
-    int preseek = (size>=chunksize) ? chunksize : size;
-    int bytesread = 0;
+    char *filename = basename(argv[1]);
+    char newfilepath[100] = "./Assignment/";
+    strcat(newfilepath, filename);
+
+    mkdir("./Assignment/", 600);
+
+    //Open file to write too
+    int fw = open(newfilepath, O_CREAT | O_WRONLY | O_APPEND, 600);
+
+    int bytesread = 0, chunksize = 5000;
+
+    //Set pointer to end of file initially
+    lseek(fd, 0, 2);
+
+    while (bytesread < size)
+    {
+        //If file is smaller than the chunksize
+        if(chunksize > size) {
+            
+            lseek(fd, 0, 0);
+
+            char *str = malloc(size);
+            read(fd, str, size);
+            
+            strrev(str, size);
+            
+            write(fw, str, size);
+            
+            free(str);
+
+            bytesread = size;
+        }
+        else {
+
+            //If you are at start of the file
+            if((size-bytesread) < chunksize) {
+                char *str = malloc(size-bytesread);
+                lseek(fd, 0, 0);
+                read(fd, str, (size-bytesread));
+                strrev(str, (size-bytesread));
+                write(fw, str, (size-bytesread));
+                free(str);
+
+                bytesread = size;
+            }
+
+            //For cases where chunksize can be read
+            else {
+                char str[5000];
+                movereadback(fd, chunksize);
+                read(fd, str, chunksize);
+                strrev(str, chunksize);
+                write(fw, str, chunksize);
+                movereadback(fd, chunksize);
+
+                bytesread += chunksize;
+            }
+        }
     
-    lseek(fd, -preseek, 2);
-
-    //Go about the chunks of blocks
-    for(int i=0; i<=size; i+=chunksize) {
-
-        //Read chunksize or <chunksize blocks?
-        int readsize = (size-i) >= chunksize ? chunksize : (size-i);
-
-        //Create string for reversal
-        char c[5000];
-        
-        
-        read(fd, c, readsize);
-        bytesread += readsize;
-
-        //Reverse the string
-        // strrev(c, readsize);
-
-        write(fw, c, readsize);
-        
-        lseek(fd, (size-bytesread) >= chunksize ? -2*chunksize : , 1)
-
-        printf("\r%f done", ((i*1.0)/size)*100);
+        char output[100];
+        sprintf(output,"\r%d percent done.\0", calcprogress((bytesread*100.0)/size));
+        write(1, output, strlen(output));
     }
 
     close(fd);
