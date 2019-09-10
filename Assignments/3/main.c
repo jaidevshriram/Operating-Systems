01b736e112d8672205cb537d73995db6761aef8c
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-09-10 20:16:53 +0530

    Started out with redirection

--- Assignments/3/main.c
+++ Assignments/3/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<fcntl.h>
 #include<string.h>
 
 //Custom header files
@@ -16,7 +17,7 @@
 
 extern char *username, hostname[100], pwd[1000];
 
-char **tokenize_input(char *input)
+char **tokenize_input(char *input, char *delimiters)
 {
 	while(iswhitespace(*input))
 		input++;
@@ -32,50 +33,21 @@ char **tokenize_input(char *input)
 
 	int position = 0;
 
-	input_part = strtok(str, " ");
+	input_part = strtok(str, delimiters);
 
 	while(input_part)
 	{
 		tokenized_input[position++] = malloc(strlen(input_part));
 		strcpy(tokenized_input[position-1], input_part);
-		input_part = strtok(NULL, " ");
+		input_part = strtok(NULL, delimiters);
 	}
 
 	return tokenized_input;
 }
-
-char **tokenize_input_semicolin(char *input)
-{
-	while(iswhitespace(*input))
-		input++;
-
-	char str[1000];
-	strcpy(str, input);
-
-	trimTrailing(str);
-
-	char **tokenized_input = (char **) malloc(1000);
-
-	char *input_part;
-
-	int position = 0;
-
-	input_part = strtok(str, ";");
-
-	while(input_part)
-	{
-		tokenized_input[position++] = malloc(strlen(input_part));
-		strcpy(tokenized_input[position-1], input_part);
-		input_part = strtok(NULL, ";");
-	}
-
-	return tokenized_input;
-}
-
 
 void start_command_execution(char *input)
 {
-	char **tokenized_input = tokenize_input(input);
+	char **tokenized_input = tokenize_input(input, "\r\n\t ");
 	char command_list[100][1000];
 	char command[100];
 
@@ -120,11 +92,85 @@ void start_command_execution(char *input)
 	add_history(input);
 }
 
+void start_redirect_handler(char *input)
+{
+	char **tokenized_input_redirect = tokenize_input(input, "<");
+	char **tokenized_output_redirect = tokenize_input(tokenized_input_redirect[0], ">");
+	char **tokenized_input = tokenize_input(tokenized_output_redirect[0], "|");
+
+	int infile = 0;
+	char infile_name[1000];
+	for(infile=0; tokenized_input_redirect[infile]!=NULL; infile++);
+
+	if(infile<=1)
+		infile = 0;
+	else
+	{
+		infile = 1;
+		strcpy(infile_name, tokenized_input_redirect[1]);
+	}
+
+	int outfile = 0;
+	char outfile_name[1000];
+	for(outfile = 0; tokenized_output_redirect[outfile]!=NULL; outfile++);
+
+	if(outfile<=1)
+		outfile = 0;
+	else
+	{
+		outfile = 1;
+		strcpy(outfile_name, tokenized_output_redirect[1]);
+	}
+
+	int tempin = dup(0);
+	int tempout = dup(1);
+
+	int fdin;
+	if(infile)
+		fdin = open(infile_name, O_RDONLY);
+	else
+		fdin = dup(tempin);
+
+	int ret, fdout, countsimple = 0;
+	for(countsimple = 0; tokenized_input[countsimple]!=NULL; countsimple++);
+
+	for (int i=0; tokenized_input[i]!=NULL; i++)
+	{
+		dup2(fdin, 0);
+		close(fdin);
+
+		if(i == countsimple - 1)
+		{
+			if(outfile)
+				fdout = open(outfile_name, O_CREAT);
+			else
+				fdout = dup(tempout);
+		}
+		else
+		{
+			int fdpipe[2];
+			pipe(fdpipe);
+			fdout = fdpipe[1];
+			fdin = fdpipe[0];
+		}
+
+		dup2(fdout, 1);
+		close(fdout);
+
+		start_command_execution(tokenized_input[i]);
+	}
+
+	dup2(tempin, 0);
+	dup2(tempout, 1);
+	close(tempin);
+	close(tempout);
+}
+
 void start_command_chain(char *input)
 {
-	char **tokenized_input = tokenize_input_semicolin(input);
+	char **tokenized_input = tokenize_input(input, ";");
 	for (int i=0; tokenized_input[i]!=NULL; i++)
-		start_command_execution(tokenized_input[i]);
+		start_redirect_handler(tokenized_input[i]);
 }
 
 int input_is_triggered()
