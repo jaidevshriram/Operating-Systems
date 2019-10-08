0680a7710c5d2a8041e71a75f24f8398b6889ae4
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-10-08 18:42:50 +0530

    Started out

---
+++ Assignments/4/2_Biryani/README.md
@@ -0,0 +1,6 @@
+# Input Format
+
+M = Robot Chef
+P = Capacity of vessel
+N = Serving Table
+K = Number of students
---
+++ Assignments/4/2_Biryani/main.c
@@ -0,0 +1,12 @@
+#include "colours.c"
+
+int number_of_robot_chef;
+int vessel_capacity;
+int number_of_serving_tables;
+int number_of_students;
+
+int main()
+{
+    //Taking input for P, V, M, K
+    scanf("%d %d %d %d", &number_of_robot_chef, &vessel_capacity, &number_of_serving_tables, &number_of_students);
+}
