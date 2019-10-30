b45dbbc1bd6a77208455db8c31d7bbbbf36cf171
Jaidev Shriram <jaidevtrumpet@gmail.com>
2019-10-31 00:27:46 +0530

    Added fields to proc struct

--- Assignments/5/proc.h
+++ Assignments/5/proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int ctime;                   // Creation Time
+  int rtime;                   // Run Time
+  int etime;                   // End Time
 };
 
 // Process memory is laid out contiguously, low addresses first:
